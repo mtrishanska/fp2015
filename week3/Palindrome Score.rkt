874e19be42f5032023c588cd3a2cf57c88e6ac5e
mtrishanska <m.trisanska@hotmail.com>
2015-11-06 16:31:35 +0200

    Create Palindrome Score.rkt

---
+++ week3/Palindrome Score.rkt
@@ -0,0 +1,15 @@
+#lang racket
+(define (reverse n)
+  (define (helper n res)
+    (cond
+      [(= n 0) res]
+      [else (helper (quotient n 10) (+ (* res 10) (remainder n 10)))]))
+  (helper n 0))
+
+(define (palindrome? n)
+  (= n (reverse n)))
+
+(define (p-score n)
+  (if (palindrome? n) 1
+      (+ 1 (p-score(+ n (reverse n))))))
+
